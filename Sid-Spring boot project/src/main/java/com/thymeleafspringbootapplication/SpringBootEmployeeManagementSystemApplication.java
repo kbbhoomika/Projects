4ac1e5b8b2a0779438cf6cf5ce2da56dac442720
kbbhoomika <165066390+kbbhoomika@users.noreply.github.com>
2024-06-03 00:01:45 +0530

    Add files via upload

---
+++ Sid-Spring boot project/src/main/java/com/thymeleafspringbootapplication/SpringBootEmployeeManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.thymeleafspringbootapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**To run springboot application**/
+public class SpringBootEmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeManagementSystemApplication.class, args);
+	}
+
+}
